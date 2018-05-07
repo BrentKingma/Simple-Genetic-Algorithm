2f70479d10ad18b10b12d55febcd55098e2dd1bf
Brent Main <kingmabrent@gmail.com>
2018-05-07 16:38:56 +1000

    Finilised Version 1.0 Added comments Merged all into one header file.

--- GA/Population.h
+++ GA/Population.h
@@ -1,14 +1,81 @@
+//-----------------------------------
+//Created by: Brent Kingma
+//Date Version 1.0 Finished: 07/05/18
+//-----------------------------------
 #pragma once
-#include "Genome.h"
 #include <vector>
 #include <functional>
-#include <ctime>
 #include <random>
+#include <ctime>
 enum class SelectionMethod
 {
 	ROULETTE = 0,
 	RANDOM,
 	TOURNAMENT
+};
+
+template<typename GeneType>
+struct Chromosome
+{
+public:
+	Chromosome() {};
+	Chromosome(int a_numGenes, std::function<GeneType()> a_geneFunc)
+	{
+		m_genes.reserve(a_numGenes);
+		for (int i = 0; i < a_numGenes; i++)
+		{
+			if (a_geneFunc == nullptr)
+			{
+				m_genes.push_back(GeneType());
+			}
+			else
+			{
+				m_genes.push_back(a_geneFunc());
+			}
+		}
+	};
+	~Chromosome() {};
+
+	std::vector<GeneType> getGenes() { return m_genes; };
+	void setGenes(std::vector<GeneType> a_newGenes) { m_genes = a_newGenes; }
+	void setGene(int a_pos, GeneType a_newGene) { m_genes[a_pos] = a_newGene; }
+	void AddGene(GeneType a_gene) { m_genes.push_back(a_gene); }
+	void Mutate(std::function<void(Chromosome<GeneType>&)> a_function)
+	{
+		a_function(*this);
+	}
+private:
+	std::vector<GeneType> m_genes;
+};
+
+template<typename GeneType>
+struct Genome
+{
+public:
+	Genome()
+	{
+		m_chromosomes.reserve(250);
+	};
+	Genome(int a_numChromosomes, int a_numGenes, std::function<GeneType()> a_geneFunc)
+	{
+		m_chromosomes.reserve(a_numChromosomes);
+		for (int i = 0; i < a_numChromosomes; i++)
+		{
+			m_chromosomes.push_back(Chromosome<GeneType>(a_numGenes, a_geneFunc));
+		}
+	};
+	~Genome() {};
+
+	Chromosome<GeneType>& GetChromosomeAtPoint(int point) { return m_chromosomes[point]; }
+	int GetNumberOfChromosomes() { return m_chromosomes.size(); }
+
+	void AddChromosome(Chromosome<GeneType>& a_chromosome) { m_chromosomes.push_back(a_chromosome); }
+	void setFitness(float a_fitness) { m_fitness = a_fitness; }
+	float GetFitness() const { return m_fitness; }
+private:
+	std::vector<Chromosome<GeneType>>	m_chromosomes;
+	bool								m_hasBeenSorted = false;
+	float								m_fitness = 0.0f;
 };
 
 template<typename GeneType>
@@ -19,6 +86,8 @@ public:
 	Population() {};
 	Population(int a_numGenomes, int a_numChromosomes, int a_numGenes, std::function<GeneType()> a_geneFunc)
 	{
+		srand((unsigned int)time(NULL));
+
 		m_numOfGenes = a_numGenes;
 		m_numOfGenomes = a_numGenomes;
 		m_numOfChromosomes = a_numChromosomes;
@@ -29,19 +98,24 @@ public:
 		}
 	};
 	~Population() {};
+	//Funtion that calculates the fitness of their Population based on user standard
 	//Remember BIGGER IS BETTER
 	void SetFitnessFunction(std::function<void(Genome<GeneType>&)> a_pointer)				{ m_fitnessFunc = a_pointer; }
+	//Function that the user created to mutate their Population to their standard
 	void SetMutateFunction(std::function<void(Chromosome<GeneType>&)> a_pointer)			{ m_mutateFunc = a_pointer; }
+	//Function that creates the genes based on the user standards
 	void SetGeneFunction(std::function<GeneType()> a_pointer)								{ m_geneFunc = a_pointer; }
 	//The array will be sorted by defualt
 	void SetDisplayFunction(std::function<void(std::vector<Genome<GeneType>>)> a_pointer)	{ m_displayFunction = a_pointer; }
+	void SetNumberOfGenomes(int a_numGenomes)												{ m_numOfGenomes = a_numGenomes; }
+	void SetNumberOfChromosomes(int a_numChromosomes)										{ m_numOfChromosomes = a_numChromosomes; }
+	void SetNumberOfGenes(int a_numGenes)													{ m_numOfGenes = a_numGenes; }
 
-	void GenerateGenomes(int a_numGenomes, int a_numChromosomes, int a_numGenes)
+	void GenerateGenomes()
 	{
-		m_numOfGenes = a_numGenes;
-		for (int i = 0; i < a_numGenomes; i++)
+		for (int i = 0; i < m_numOfGenomes; i++)
 		{
-			m_genomes.push_back(Genome<DataType>(a_numChromosomes, a_numGenes, m_geneFunc));
+			m_genomes.push_back(Genome<GeneType>(m_numOfChromosomes, m_numOfGenes, m_geneFunc));
 		}
 	}
 	void SelectionAndBreeding(SelectionMethod a_selection)
@@ -206,4 +280,7 @@ private:
 		return score;
 	}
 };
+
+
+
 
